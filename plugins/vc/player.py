44f2b34caed199bb8584fe087464f8155eb4399a
TGBotSzK <77600757+ZauteKm@users.noreply.github.com>
2021-04-20 20:21:18 +0530

    Update player.py

--- plugins/vc/player.py
+++ plugins/vc/player.py
@@ -74,7 +74,7 @@ __starts with ! (exclamation mark)__
 
 USERBOT_REPO = f"""{emoji.ROBOT} **Telegram Voice Chat UserBot**
 
-- Repository: [GitHub](https://github.com/callsmusic/tgvc-userbot)
+- Repository: [GitHub](https://github.com/ZauTeKm/tg-vc-userbot)
 - License: AGPL-3.0-or-later"""
 
 
