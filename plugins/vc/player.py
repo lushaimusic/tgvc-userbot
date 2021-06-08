b20f23378b3e469c0a0c30141b862874769eab72
Mr. Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-06-08 15:33:01 +0530

    Update player.py

--- plugins/vc/player.py
+++ plugins/vc/player.py
@@ -77,7 +77,7 @@ __starts with ! (exclamation mark)__
 
 USERBOT_REPO = f"""{emoji.ROBOT} **Telegram Voice Chat UserBot**
 
-- Repository: [GitHub](https://github.com/callsmusic/tgvc-userbot)
+- Repository: [GitHub](https://github.com/infojostel/tg-vc-userbot)
 - License: AGPL-3.0-or-later"""
 
 # - Pyrogram filters
