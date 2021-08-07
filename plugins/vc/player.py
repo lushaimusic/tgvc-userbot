b01b495f8dc984c7c6dd308d3101cdb7035595b1
Puutraaa <88121146+Puutraaa@users.noreply.github.com>
2021-08-07 09:20:17 +0800

    Update player.py

--- plugins/vc/player.py
+++ plugins/vc/player.py
@@ -151,9 +151,9 @@ mp = MusicPlayer()
 async def network_status_changed_handler(context, is_connected: bool):
     if is_connected:
         mp.chat_id = MAX_CHANNEL_ID - context.full_chat.id
-        await send_text(f"{emoji.CHECK_MARK_BUTTON} joined the voice chat")
+        await send_text(f"{emoji.CHECK_MARK_BUTTON}  NAIK KE BATLLE DESAH")
     else:
-        await send_text(f"{emoji.CROSS_MARK_BUTTON} left the voice chat")
+        await send_text(f"{emoji.CROSS_MARK_BUTTON} MALES AHH SUARANYA JELE")
         mp.chat_id = None
 
 
