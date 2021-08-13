61469a96982f4b107a4955d9ccdf5449f52ca75e
Puutraaa <88121146+Puutraaa@users.noreply.github.com>
2021-08-13 10:20:54 +0800

    Update userbot.py

--- userbot.py
+++ userbot.py
@@ -1,7 +1,20 @@
 # import logging
 from pyrogram import Client, idle
 
-app = Client("tgvc")
+API_ID = int(environ["API_ID"])
+API_HASH = environ["API_HASH"]
+SESSION_NAME = environ["SESSION_NAME"]
+
+PLUGINS = dict(
+    root="plugins",
+    include=[
+        "vc." + environ["PLUGIN"],
+        "ping",
+        "sysinfo"
+    ]
+)
+
+app = Client(SESSION_NAME, API_ID, API_HASH, plugins=PLUGINS)
 # logging.basicConfig(level=logging.INFO)
 app.start()
 print('>>> USERBOT STARTED')
